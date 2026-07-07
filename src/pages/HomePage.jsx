1397c6e3cff60497ebcc09564d68982e01b23110
Pilily <braian2024fernandez26@gmail.com>
2026-07-07 13:04:24 -0300

    Comment out jineteada image in HomePage
    Comment out the jineteada image import and usage.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeaderComponent from "../components/HeaderComponent";
 import logo from "/public/pago.png";
-import jineteada from "/IMG_5839.png";
+// import jineteada from "/IMG_5839.png";
 
 
 const HomePage = () => {
@@ -10,7 +10,7 @@ const HomePage = () => {
       <HeaderComponent></HeaderComponent>
       <h1>Bienvenidos nuevo mi sito web, Pueden buscar las fotos y me envian</h1>
       <img src={logo} alt="" width={400} className="pago" />
-      <img src={jineteada} alt="" width={400} />
+      {/* <img src={jineteada} alt="" width={400} /> */}
     </div>
   );
 };
