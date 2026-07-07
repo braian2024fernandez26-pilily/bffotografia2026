d4c9dd36fb05fda98dba3c59395f3b8de8281d0e
Pilily <braian2024fernandez26@gmail.com>
2026-07-07 12:47:33 -0300

    Remove image URLs from BlogPage
    Commented out image URLs in the BlogPage component.

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -7,13 +7,13 @@ const BlogPage = () => {
   //   {
   //     id: "1",
   //     name: "Captura de pantalla",
-  //     img: "https://lh3.googleusercontent.com/pw/AP1GczM_l_26di161wPPTdw-ig6DtZHiuCZjPsPK85VJqFyubVQBJWvMk9xadA4PuHIQxluSiBcJe-gUfNtIA_pokzML-VDAGLDGwD79eRGR3huQxUZNJVmBp6ugawzjAzT0I07EkAuH4JgUBiERhdWXyEFz=w720-h720-s-no-gm?authuser=0",
+  //     img: "",
   //     title: "Comprar Foto",
   //   },
   //   {
   //     id: "2",
   //     name: "Captura de pantalla",
-  //     img: "https://lh3.googleusercontent.com/pw/AP1GczPv_hpkb1pkzFXNWXZeN7zHEOuJ-h6Z06H1rRyJRAGnZuqxts8uMRY4b9xQktqOPW37OQ5iBGLXUCaBrURi34hsYUVCD7lv6ZM6_ZdwH3rx5CmDkYUXPUx8zfKNV25owR9szL4YkpkY4HPrIdam5cCY=w735-h919-s-no-gm?authuser=0",
+  //     img: "",
   //     title: "Comprar Foto",
   //   },
   // ];
