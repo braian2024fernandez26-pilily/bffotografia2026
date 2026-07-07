4bab29fef0dd20857939bb12bb57da24a35c878f
Pilily <braian2024fernandez26@gmail.com>
2026-07-07 17:48:35 -0300

    Change footer element from small to paragraph

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
       <h1>Bienvenidos nuevo mi sito web, Pueden buscar las fotos y me envian</h1>
       <img src={jineteada} alt="" width={400} className="pago" />
       <img src={logo} alt="" width={400} className="pago" />
-      <small>&copy; 2026 <b>BF Fotografia Gober,Castro</b></small>
+      <p>&copy; 2026 <b>BF Fotografia Gober,Castro</b></p>
       <p>Publicado: 7 de Julio</p>
     </div>
   );
