11b933bb42051286446d9ad002e228edb3d85a67
Pilily <braian2024fernandez26@gmail.com>
2026-07-07 12:46:01 -0300

    Comment out sordos data and postCard mapping

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -3,32 +3,33 @@ import HeaderComponent from "../components/HeaderComponent";
 import PostCard from "../components/PostCard";
 
 const BlogPage = () => {
-  const sordos = [
-    {
-      id: "1",
-      name: "Captura de pantalla",
-      img: "https://lh3.googleusercontent.com/pw/AP1GczM_l_26di161wPPTdw-ig6DtZHiuCZjPsPK85VJqFyubVQBJWvMk9xadA4PuHIQxluSiBcJe-gUfNtIA_pokzML-VDAGLDGwD79eRGR3huQxUZNJVmBp6ugawzjAzT0I07EkAuH4JgUBiERhdWXyEFz=w720-h720-s-no-gm?authuser=0",
-      title: "Comprar Foto",
-    },
-    {
-      id: "2",
-      name: "Captura de pantalla",
-      img: "https://lh3.googleusercontent.com/pw/AP1GczPv_hpkb1pkzFXNWXZeN7zHEOuJ-h6Z06H1rRyJRAGnZuqxts8uMRY4b9xQktqOPW37OQ5iBGLXUCaBrURi34hsYUVCD7lv6ZM6_ZdwH3rx5CmDkYUXPUx8zfKNV25owR9szL4YkpkY4HPrIdam5cCY=w735-h919-s-no-gm?authuser=0",
-      title: "Comprar Foto",
-    },
-  ];
+  // const sordos = [
+  //   {
+  //     id: "1",
+  //     name: "Captura de pantalla",
+  //     img: "https://lh3.googleusercontent.com/pw/AP1GczM_l_26di161wPPTdw-ig6DtZHiuCZjPsPK85VJqFyubVQBJWvMk9xadA4PuHIQxluSiBcJe-gUfNtIA_pokzML-VDAGLDGwD79eRGR3huQxUZNJVmBp6ugawzjAzT0I07EkAuH4JgUBiERhdWXyEFz=w720-h720-s-no-gm?authuser=0",
+  //     title: "Comprar Foto",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Captura de pantalla",
+  //     img: "https://lh3.googleusercontent.com/pw/AP1GczPv_hpkb1pkzFXNWXZeN7zHEOuJ-h6Z06H1rRyJRAGnZuqxts8uMRY4b9xQktqOPW37OQ5iBGLXUCaBrURi34hsYUVCD7lv6ZM6_ZdwH3rx5CmDkYUXPUx8zfKNV25owR9szL4YkpkY4HPrIdam5cCY=w735-h919-s-no-gm?authuser=0",
+  //     title: "Comprar Foto",
+  //   },
+  // ];
 
-  const postCard = sordos.map((sord) => {
-    return (
-      <li key={sord.id}>
-        <PostCard sord={sord}></PostCard>
-      </li>
-    );
-  });
+  // const postCard = sordos.map((sord) => {
+  //   return (
+  //     <li key={sord.id}>
+  //       <PostCard sord={sord}></PostCard>
+  //     </li>
+  //   );
+  // });
   return (
     <div>
       <HeaderComponent></HeaderComponent>
-      {postCard}
+      <h1></h1>
+      /* {postCard} */
     </div>
   );
 };
