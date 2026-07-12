1e6b125b3a743577de85f752755fb2a6b3683f00
Pilily <braian2024fernandez26@gmail.com>
2026-07-12 11:21:08 -0300

    Comment out welcome message and jineteada image
    Comment out the welcome message and image for jineteada.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import HeaderComponent from "../components/HeaderComponent";
 import logo from "/public/pago.png";
-import jineteada from "/public/jineteada.jpg";
+// import jineteada from "/public/jineteada.jpg";
 
 const HomePage = () => {
   return (
     <div>
       <HeaderComponent></HeaderComponent>
-      <h1>Bienvenidos nuevo mi sito web, Pueden buscar las fotos y me envian</h1>
-      <img src={jineteada} alt="" width={400} className="pago" />
+      {/* <h1>Bienvenidos nuevo mi sito web, Pueden buscar las fotos y me envian</h1> */}
+      {/* <img src={jineteada} alt="" width={400} className="pago" /> */}
       <img src={logo} alt="" width={400} className="pago" />
       <p>&copy; 2026 <b>BF Fotografia Gober,Castro</b></p>
       <p>Publicado: 10 de Julio</p>
